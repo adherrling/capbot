2a0f7dae3669f11fa5375644513de8786c80087f
Austin <adherrling@gmail.com>
2017-11-11 14:45:06 -0500

    Added a couple extra request features, randomized list

--- capbot.py
+++ capbot.py
@@ -2,6 +2,7 @@
 """Reads in clan data html and parses out the list of clan members."""
 import time
 from html.parser import HTMLParser
+from random import shuffle
 import argparse
 import datetime
 import requests
@@ -50,6 +51,7 @@ class MyHTMLParser(HTMLParser):
                 add_item = item[1:-1]
                 add_item = add_item.replace(u'\xa0', u' ')
                 clan_list.append(add_item)
+            shuffle(clan_list)
             self.data = clan_list
 
 def check_cap(user):
@@ -76,7 +78,7 @@ def add_cap_to_db(clan_list):
     add_list = []
     capped_users = []
     for user in clan_list:
-        time.sleep(1)
+        time.sleep(3)
         cap_date = check_cap(user)
         if cap_date is not None:
             cap_date = datetime.datetime.strptime(cap_date, "%a, %d %b %Y %H:%M:%S %Z")
@@ -98,7 +100,7 @@ def add_cap_to_db(clan_list):
                 account_record = Account(**account_dict)
                 add_list.append(upsert(Account, primary_key_map, account_record))
                 print(f"{user} last capped at the citadel on {cap_date}.")
-                capped_users.append(user)
+                capped_users.append((user, cap_date))
         else:
             print(f"{user} has not capped at the citadel.")
 
@@ -145,6 +147,7 @@ def main():
     parser = argparse.ArgumentParser(
         description="Choose to check for new caps or zero out existing caps.")
     parser.add_argument("-c", "--check", help="Runs the cap check and bot", action="store_true")
+    parser.add_argument("-b", "--bot", help="Runs only the bot", action="store_true")
     # parser.add_argument("-r", "--reset", help="Zeros out existing caps", action="store_true")
     parser.add_argument("-i", "--init", help="Reinitializes the database", action="store_true")
     args = parser.parse_args()
@@ -167,6 +170,12 @@ def main():
         run_bot(capped_users, token)
     elif args.init:
         init_db()
+    elif args.bot:
+        token = ""
+        with open("/home/austin/Documents/capbot/token.txt", "r") as tokenfile:
+            token = tokenfile.read().strip()
+        run_bot([], token)
+
 
 def run_bot(capped_users, token):
     """Actually runs the bot"""
@@ -184,9 +193,10 @@ def run_bot(capped_users, token):
         for server in servers:
             def_chan = server.default_channel
         user_updates = ""
-        for user in capped_users:
+        for (user, cap_date) in capped_users:
             curdate = datetime.datetime.now(datetime.timezone.utc).strftime("%a %b %d, %H:%M:%S")
-            user_updates += f"{user} has capped at the citadel at approximately: {curdate}.\n"
+            user_updates += f"Cap: {user} has capped at the citadel at approximately: {curdate}.\n"
+            user_updates += f"Adventurer's Log time: {cap_date}.\n"
         if user_updates != "":
             await client.send_message(def_chan, user_updates)
 
@@ -201,16 +211,55 @@ def run_bot(capped_users, token):
     async def on_message(message):
         """Handles commands based on messages sent"""
         if message.content.startswith('!caplist'):
-            pass
-            # capped_users = session.query(
-            #     Account.name).filter(Account.capped_this_week == True).all()
-            # users = []
-            # for user in capped_users:
-            #     users.append(user[0])
-            # output = "\n".join(users)
-            # await client.send_message(message.channel, output)
-        if message.content.startswith('!vis'):
+            capped_users = session.query(Account.name).all()
+            users = []
+            for user in capped_users:
+                users.append(user[0])
+            ret_str = ""
+            for i in range(len(users)):
+                ret_str += f"{i+1}. {users[i]}\n"
+            await client.send_message(message.channel, ret_str)
+
+        elif message.content.startswith('!vis'):
             await client.send_message(message.channel, "Fuck you")
+
+        elif message.content.startswith('!delmsgs'):
+            role_list = [role.name for role in message.author.roles]
+            if "cap handler" in role_list:
+                info = message.content.split(" ")[1]
+                if info == "all":
+                    async for msg in client.logs_from(message.channel, limit=500):
+                        if msg.author == client.user:
+                            await client.delete_message(msg)
+                else:
+                    # Try to interpret info as a message id. Thankfully bots fail gracefully
+                    before_msg = await client.get_message(message.channel, info)
+                    async for msg in client.logs_from(
+                            message.channel, limit=500, before=before_msg):
+                        if msg.author == client.user:
+                            await client.delete_message(msg)
+
+        elif message.author.name == "Roscroft" and message.channel.is_private:
+            if not message.content == "Roscroft":
+                await client.send_message(discord.Object(id='350087804564275200'),
+                                          message.content)
+
+        elif message.content.startswith('!help'):
+            await client.send_message(
+                message.channel, ("Use !delmsgs <id> do delete all messages before the provided "
+                                  "message id. Use '!delmsgs all' to delete all bot messages."))
+
+        elif message.content.startswith('!list'):
+            userlist = []
+            async for msg in client.logs_from(message.channel, limit=500):
+                if msg.author == client.user and msg.content.startswith("Cap:"):
+                    user_name = msg.content.split(" ")[1]
+                    userlist.append(user_name)
+            print(userlist)
+            ret_str = ""
+            for i in range(len(userlist)):
+                ret_str += f"{i+1}. {userlist[i]}\n"
+            await client.send_message(message.channel, ret_str)
 
     client.run(token)
 
