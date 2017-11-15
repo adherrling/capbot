37fd7bf9a860bb9fffa68039399777ee5e115ddc
Austin <adherrling@gmail.com>
2017-11-15 14:56:41 -0500

    Found a way to use JSONs. Way faster and more accurate now.

--- capbot.py
+++ capbot.py
@@ -1,37 +1,29 @@
 #!/usr/bin/python3.6
 """Reads in clan data html and parses out the list of clan members."""
-import time
 from html.parser import HTMLParser
 from random import shuffle
 import argparse
 import datetime
+import json
 import requests
-import feedparser
 import discord
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import SESSIONmaker
 
 ENGINE = create_engine(f"sqlite:////home/austin/Documents/capbot/clancaps.db")
-SESSION = sessionmaker(bind=ENGINE)
+MASTER_SESSION = SESSIONmaker(bind=ENGINE)
 BASE = declarative_base()
-
-session = SESSION()
+REQUEST_SESSION = requests.SESSION()
+SESSION = MASTER_SESSION()
 
 class Account(BASE):
     """Defines the class to handle account names and historical caps"""
     __tablename__ = 'account'
     name = Column(String(50), primary_key=True)
-    last_cap_reported = Column(DateTime)
-    last_cap_actual = Column(DateTime)
+    last_cap_time = Column(DateTime)
     total_caps = Column(Integer)
-
-# class LastReset(BASE):
-#     """Stores the date of the last reset, for comparison"""
-#     __tablename__ = "lastReset"
-#     lid = Column(Integer, primary_key=True)
-#     last_reset = Column(DateTime)
 
 def init_db():
     """Initialized and optionally clears out the database"""
@@ -54,23 +46,44 @@ class MyHTMLParser(HTMLParser):
             shuffle(clan_list)
             self.data = clan_list
 
+# def old_check_cap(user):
+#     """Given a feed url, returns true if the cap message is present."""
+#     feed_str = ""
+#     with open("/home/austin/Documents/capbot/url.txt", "r") as url_file:
+#         feed_str += url_file.read().strip()
+#     feed_str += user
+#     print(feed_str)
+#     user_feed = feedparser.parse(feed_str)
+#     try:
+#         print(user_feed['feed']['title'])
+#     except KeyError:
+#         print("User profile private.")
+#         return None
+#     for entry in user_feed['entries']:
+#         if entry.title == "Capped at my Clan Citadel.":
+#             print(f"{user} has capped.")
+#             return entry.published
+#     return None
+
 def check_cap(user):
-    """Given a feed url, returns true if the cap message is present."""
-    feed_str = ""
+    """Given a user, return cap date if it is in their activity history."""
+    url_str = ""
     with open("/home/austin/Documents/capbot/url.txt", "r") as url_file:
-        feed_str += url_file.read().strip()
-    feed_str += user
-    print(feed_str)
-    user_feed = feedparser.parse(feed_str)
+        url_str = url_file.read().strip()
+    url_str += user
+    url_str += "&activities=20"
+    data = REQUEST_SESSION.get(url_str).content
+    data_json = json.loads(data)
     try:
-        print(user_feed['feed']['title'])
+        activities = data_json['activities']
     except KeyError:
-        print("User profile private.")
+        print(f"{user}'s profile is private.")
         return None
-    for entry in user_feed['entries']:
-        if entry.title == "Capped at my Clan Citadel.":
-            print(f"{user} has capped.")
-            return entry.published
+    for activity in activities:
+        if "capped" in activity['details']:
+            date = activity['date']
+            print(f"{user} capped on {date}.")
+            return activity['date']
     return None
 
 def add_cap_to_db(clan_list):
@@ -78,14 +91,14 @@ def add_cap_to_db(clan_list):
     add_list = []
     capped_users = []
     for user in clan_list:
-        time.sleep(2)
         cap_date = check_cap(user)
         if cap_date is not None:
-            cap_date = datetime.datetime.strptime(cap_date, "%a, %d %b %Y %H:%M:%S %Z")
+            cap_date = datetime.datetime.strptime(cap_date, "%d-%b-%Y %H:%M")
+            # cap_date = datetime.datetime.strptime(cap_date, "%a, %d %b %Y %H:%M:%S %Z")
             # If the cap date is not None, that means the user has a cap in their adventurer's log.
             # We need to do a few things. First, check to see if the cap date is already stored in
             # the database under last_cap_reported
-            previous_report = session.query(
+            previous_report = SESSION.query(
                 Account.last_cap_reported).filter(Account.name == user).first()
             # Two outcomes: previous report is None, or it has a value. If it is none, then we
             # update it to be cap_date, and store the current time as last_cap_actual.
@@ -105,55 +118,56 @@ def add_cap_to_db(clan_list):
             print(f"{user} has not capped at the citadel.")
 
     add_list = [item for item in add_list if item is not None]
-    session.add_all(add_list)
-    session.commit()
+    SESSION.add_all(add_list)
+    SESSION.commit()
     return capped_users
 
 def upsert(table, primary_key_map, obj):
     """Decides whether to insert or update an object."""
-    first = session.query(table).filter_by(**primary_key_map).first()
+    first = SESSION.query(table).filter_by(**primary_key_map).first()
     if first != None:
         keys = table.__table__.columns.keys()
-        session.query(table).filter_by(**primary_key_map).update(
+        SESSION.query(table).filter_by(**primary_key_map).update(
             {column: getattr(obj, column) for column in keys})
         return None
     return obj
 
 # def erase_caps():
 #     """Run this to zero out the caps."""
-#     users = session.query(Account).all()
+#     users = SESSION.query(Account).all()
 #     for user in users:
-#         total = session.query(Account.total_caps).filter(Account.name == user.name).first()[0]
-#         capped = session.query(
+#         total = SESSION.query(Account.total_caps).filter(Account.name == user.name).first()[0]
+#         capped = SESSION.query(
 #             Account.capped_this_week).filter(Account.name == user.name).first()[0]
 #         if total is None:
 #             total = 0
 #         if capped:
-#             session.query(Account).filter(
+#             SESSION.query(Account).filter(
 #                 Account.name == user.name).update({Account.total_caps: total+1})
 #     for user in users:
-#         session.query(Account).filter(
+#         SESSION.query(Account).filter(
 #             Account.name == user.name).update({Account.capped_this_week: False})
 #     primary_key_map = {"lid": 1}
 #     reset_dict = {"lid": 1, "last_reset": datetime.datetime.now()}
 #     reset = LastReset(**reset_dict)
 #     reset_obj = upsert(LastReset, primary_key_map, reset)
 #     if reset_obj is not None:
-#         session.add(reset_obj)
-#     session.commit()
+#         SESSION.add(reset_obj)
+#     SESSION.commit()
 
 def main():
     """Runs the stuff."""
     parser = argparse.ArgumentParser(
         description="Choose to check for new caps or zero out existing caps.")
     parser.add_argument("-c", "--check", help="Runs the cap check and bot", action="store_true")
+    parser.add_argument("-u", "--update", help="Runs only the cap check", action="store_true")
     parser.add_argument("-b", "--bot", help="Runs only the bot", action="store_true")
     # parser.add_argument("-r", "--reset", help="Zeros out existing caps", action="store_true")
     parser.add_argument("-i", "--init", help="Reinitializes the database", action="store_true")
     args = parser.parse_args()
     # if args.reset:
     #     erase_caps()
-    if args.check:
+    if args.check or args.update:
         clan_parser = MyHTMLParser()
         url_str = ""
         with open("/home/austin/Documents/capbot/clanlist.txt", "r") as url_file:
@@ -167,7 +181,8 @@ def main():
         token = ""
         with open("/home/austin/Documents/capbot/token.txt", "r") as tokenfile:
             token = tokenfile.read().strip()
-        run_bot(capped_users, token)
+        if args.check:
+            run_bot(capped_users, token)
     elif args.init:
         init_db()
     elif args.bot:
@@ -212,14 +227,15 @@ def run_bot(capped_users, token):
     async def on_message(message):
         """Handles commands based on messages sent"""
         if message.content.startswith('!caplist'):
-            capped_users = session.query(Account.name).all()
-            users = []
-            for user in capped_users:
-                users.append(user[0])
-            ret_str = ""
-            for i in range(len(users)):
-                ret_str += f"{i+1}. {users[i]}\n"
-            await client.send_message(message.channel, ret_str)
+            pass
+            # capped_users = SESSION.query(Account.name).all()
+            # users = []
+            # for user in capped_users:
+            #     users.append(user[0])
+            # ret_str = ""
+            # for i in range(len(users)):
+            #     ret_str += f"{i+1}. {users[i]}\n"
+            # await client.send_message(message.channel, ret_str)
 
         elif message.content.startswith('!vis'):
             await client.send_message(message.channel, "Fuck you")
